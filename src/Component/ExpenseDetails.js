2382f4f0943c5a95456318200be83effdf1fc457
SourabhK0625 <sourabh061999@gmail.com>
2022-06-29 18:41:39 +0530

    Updated

--- src/Component/ExpenseDetails.js
+++ src/Component/ExpenseDetails.js
@@ -2,7 +2,7 @@ import './ExpenseItem.css';
 function ExpenseDetails(props)
 {
     return (
-        <div className="expense-item__description">
+    <div className="expense-item__description">
         <h2>{props.title}</h2>
         <div>
             <h3 className='expense-item__price'> Rs {props.amount}</h3>
--- src/Component/ExpenseItem.js
+++ src/Component/ExpenseItem.js
@@ -8,9 +8,7 @@ function ExpenseItem(props)
     return(
       <div  className="expense-item">
         <ExpenseDate date = {props.date} />
-        <div className="expense-item__description">
-          <ExpenseDetails title = {props.title} amount= {props.amount} location = {props.location} />
-        </div>
+        <ExpenseDetails title = {props.title} amount= {props.amount} location = {props.location} />
       </div>
   );
 }
