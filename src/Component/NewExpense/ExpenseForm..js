899024767de1949a2d6813e934341563778642f8
SourabhK0625 <sourabh061999@gmail.com>
2022-07-01 14:01:40 +0530

    UseState cases

--- src/Component/NewExpense/ExpenseForm..js
+++ src/Component/NewExpense/ExpenseForm..js
@@ -1,10 +1,47 @@
-import React from "react";
+import React,{useState} from "react";
 import './ExpenseForm.css';
 const ExpenseForm = () =>
 {
+    // const [enteredTitle , setEnteredTitle] = useState('')
+    // const [enteredAmount , setEnteredAmount] = useState('')
+    // const [enteredDate, setEnteredDate] = useState('')
+    const [userInput , setUserInput] = useState(
+        {
+            enteredTitle : '',
+            enteredAmount : '',
+            enteredDate : ''
+        }
+    )
+
     const titleChangeHandler = (event) =>
     {
-        console.log(event.target.value);
+        // setEnteredTitle(event.target.value);
+        setUserInput(
+            {
+                ...userInput , 
+                enteredTitle : event.target.value,
+            }
+        );
+    }
+    const amountChangeHandler = (event) =>
+    {
+        // setEnteredAmount(event.target.value);
+        setUserInput(
+            {
+                ...userInput , 
+                enteredAmount : event.target.value,
+            }
+        );
+    }
+    const dateChangeHandler = (event) =>
+    {
+        // setEnteredDate(event.target.value);
+        setUserInput(
+            {
+                ...userInput , 
+                enteredDate : event.target.value,
+            }
+        );
     }
 
 
@@ -16,12 +53,12 @@ const ExpenseForm = () =>
                     <input type='text'></input> 
                 </div>
 
-                <div className="new-expense__control">
+                <div className="new-expense__control" onChange={amountChangeHandler}>
                     <label>Amount</label>
                     <input type='number' min="0.01" step="0.01"></input> 
                 </div>
 
-                <div className="new-expense__control">
+                <div className="new-expense__control" onChange={dateChangeHandler}>
                     <label>Date</label>
                     <input type='date' min="2019-01-01" max="2022-12-31"></input> 
                 </div>
