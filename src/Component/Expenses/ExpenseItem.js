1eeb58df907f42780e83599642e7fdfcf8f1a63b
SourabhK0625 <sourabh061999@gmail.com>
2022-07-01 14:35:35 +0530

    Submit form

--- src/Component/Expenses/ExpenseItem.js
+++ src/Component/Expenses/ExpenseItem.js
@@ -18,7 +18,7 @@ const ExpenseItem = (props) => {
       <div className='expense-item__description'>
         <h2>{props.title}</h2>
         <h2>{amount}</h2>
-        <div className='expense-item__price'>{amount}</div>
+        <div className='expense-item__price'>${amount}</div>
       </div>
       <button onClick={clickHandler}>Expense</button>
     </Card>
