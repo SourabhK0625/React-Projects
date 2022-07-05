34679ee049f6f61d5425f82e8c24bc10fc7f3846
SourabhK0625 <sourabh061999@gmail.com>
2022-07-05 14:38:11 +0530

    Adding chart

--- src/Component/Expenses/ExpenseItem.js
+++ src/Component/Expenses/ExpenseItem.js
@@ -1,28 +1,21 @@
-import React, {useState} from 'react';
+import React from 'react';
 
 import ExpenseDate from './ExpenseDate';
 import Card from '../UI/Card';
 import './ExpenseItem.css';
 
-
 const ExpenseItem = (props) => {
-  const [amount , letAmount] = useState(props.amount)
-  const clickHandler = () =>
-  {
-    letAmount('100')
-    console.log('Deleted...')
-  }
   return (
-    <Card className='expense-item'>
-      <ExpenseDate date={props.date} />
-      <div className='expense-item__description'>
-        <h2>{props.title}</h2>
-        <h2>{amount}</h2>
-        <div className='expense-item__price'>${amount}</div>
-      </div>
-      <button onClick={clickHandler}>Expense</button>
-    </Card>
+    <li>
+      <Card className='expense-item'>
+        <ExpenseDate date={props.date} />
+        <div className='expense-item__description'>
+          <h2>{props.title}</h2>
+          <div className='expense-item__price'>${props.amount}</div>
+        </div>
+      </Card>
+    </li>
   );
-}
+};
 
 export default ExpenseItem;
