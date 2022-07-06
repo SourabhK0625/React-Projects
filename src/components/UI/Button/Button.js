c868965e7c20fb80591ecba924dc4c854911fef0
SourabhK0625 <sourabh061999@gmail.com>
2022-07-06 11:55:08 +0530

    Button Change

--- src/components/UI/Button/Button.js
+++ src/components/UI/Button/Button.js
@@ -4,7 +4,8 @@ import './Button.css';
 
 const Button = props => {
   return (
-    <button type={props.type} className="button" onClick={props.onClick}>
+
+    <button style={{background: !props.displayStyle ? 'salmon':'#8b005d'}} type={props.type} className="button" onClick={props.onClick}>
       {props.children}
     </button>
   );
