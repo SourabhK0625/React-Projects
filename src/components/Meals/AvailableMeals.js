225195f52213fe00a51f5eef5d0207767e6e71dc
SourabhK0625 <sourabh061999@gmail.com>
2022-07-11 18:46:51 +0530

    Array list

--- src/components/Meals/AvailableMeals.js
+++ src/components/Meals/AvailableMeals.js
@@ -1,5 +1,6 @@
 import classes from './AvailableMeals.module.css';
 import Card from '../UI/Card';
+import MealItem from './MealItem/MealItem';
 const DUMMY_MEALS = [
     {
       id: 'm1',
@@ -11,7 +12,7 @@ const DUMMY_MEALS = [
       id: 'm2',
       name: 'Schezwan Egg Fried Rice',
       description: 'A chinese fried rice!',
-      price: 16.5,
+      price: 180,
     },
     {
       id: 'm3',
@@ -29,7 +30,11 @@ const DUMMY_MEALS = [
 
   const AvailableMeals = () =>
   {
-    const mealsList = DUMMY_MEALS.map(meal => <li>{meal.name}</li>);
+    const mealsList = DUMMY_MEALS.map(meal => <MealItem key={meal.id} 
+      name={meal.name} 
+      description={meal.description} 
+      price={meal.price}/>
+      );
     return <section className={classes.meals}>
         <Card>
           <ul>{mealsList}</ul>  
--- src/components/Meals/MealItem/MealItem.js
+++ src/components/Meals/MealItem/MealItem.js
@@ -1,8 +1,13 @@
 import classes from './MealItem.module.css';
 
 const MealItem = props =>{
-    return <li>
-        <div><h3>{props.name}</h3></div>
+    const price = `Rs ${props.price.toFixed(2)}`
+    return <li className={classes.meal}>
+        <div>
+            <h3>{props.name}</h3>
+            <div className={classes.description}>{props.description}</div>
+            <div className={classes.price}>{price}</div>
+        </div>
         <div></div>
     </li>
 };
