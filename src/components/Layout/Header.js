1d120c5032f937d74bc9afd2c28d9a37586ab962
SourabhK0625 <sourabh061999@gmail.com>
2022-07-13 18:25:41 +0530

    Open Close

--- src/components/Layout/Header.js
+++ src/components/Layout/Header.js
@@ -7,7 +7,7 @@ const Header = props => {
     return <Fragment>
         <header className={classes.header}>
             <h1>Hotel Shree Panjurli.....!</h1>
-            <HeaderCartButton />
+            <HeaderCartButton onClick={props.onShowCart} />
         </header>
         <div className={classes['main-image']}>
             <img src={meals} alt="When not in Mood, Just Have Some Foooood......!"></img>
