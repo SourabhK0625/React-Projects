e9c3846e42b52ecc9e04e941fce21148215b212b
SourabhK0625 <106949506+SourabhK0625@users.noreply.github.com>
2022-07-21 13:31:43 +0530

    Update

--- src/components/Auth/AuthForm.js
+++ src/components/Auth/AuthForm.js
@@ -1,78 +1,77 @@
-import { useState, useRef } from "react";
+import { useState, useRef } from 'react';
 
-import classes from "./AuthForm.module.css";
+import classes from './AuthForm.module.css';
 
 const AuthForm = () => {
-  const emailInputRef = useRef();
-  const emailPasswordRef = useRef();
   const [isLogin, setIsLogin] = useState(true);
-  const [isLoading , setIsLoading] = useState(false);
-  
+  const [isLoading, setIsLoading] = useState(false);
+  const emailInputRef = useRef();
+  const passwordInputRef = useRef();
 
   const switchAuthModeHandler = () => {
     setIsLogin((prevState) => !prevState);
   };
 
-  const submitHandler = (event) => {
+  const submitHandler = (event)=>{
     event.preventDefault();
-    const enteredEmail = emailInputRef.current.value;
-    const enteredPassword = emailPasswordRef.current.value;
-
+    const enteredEmail = emailInputRef.current.value
+    const enteredPassword  = passwordInputRef.current.value
+    console.log(enteredEmail,enteredPassword)
     setIsLoading(true);
 
-    if (isLogin) {
-    } else {
-      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCJXTxViMQjMdFrJPUAo4CFUgeDKNENucE',
-        {
-          method: "POST",
-          body: JSON.stringify({
-            email: enteredEmail,
-            password: enteredPassword,
-            returnSecureToken: true,
-          }),
-          headers: { "Content-Type": "application/json" },
-        }
-      ).then((res) => 
-      {
-        setIsLoading(false);
-        if (res.ok) {
-        } else {
-          return res.json().then((data) => {
-            console.log(data);
-            let errorMessage = 'Autentication Failed !';
-            alert(errorMessage)
-          });
-        }
-      });
+    if(isLogin){
+
     }
+    else{
+      fetch('https://identitytoolkit.googleapis.com/v1/accounts:signUp?key=AIzaSyCJXTxViMQjMdFrJPUAo4CFUgeDKNENucE',
+      {
+        method:'POST',
+        body:JSON.stringify(
+          {
+            email:enteredEmail,
+            password: enteredPassword,
+            returnSecureToken: true
+          }
+        ),
+        headers:{
+          'Content-Type': 'application/json'
+        }
+      }).then(res=>{
+        setIsLoading(false);
+        if(res.ok){
+
+        }else{
+          return res.json().then(data=>{
+            alert('Authentication failed!!')
+          })
+        }
+      })
+      
+    }
+
   };
 
   return (
     <section className={classes.auth}>
-      <h1>{isLogin ? "Login" : "Sign Up"}</h1>
-      <form onClick={submitHandler}>
-        <div className={classes.control}>
-          <label htmlFor="email">Your Email</label>
-          <input type="email" id="email" required ref={emailInputRef} />
+      <h1>{isLogin ? 'Login' : 'Sign Up'}</h1>
+      <form onSubmit={submitHandler}>
+        <div className={classes.control} >
+          <label htmlFor='email'>Your Email</label>
+          <input type='email' id='email' required ref={emailInputRef} />
         </div>
         <div className={classes.control}>
-          <label htmlFor="password">Your Password</label>
-          <input
-            type="password"
-            id="password"
-            required
-            ref={emailPasswordRef}
-          />
+          <label htmlFor='password'>Your Password</label>
+          <input type='password' id='password' required ref={passwordInputRef} />
         </div>
         <div className={classes.actions}>
-          {!isLoading && <button>{isLogin ? "Login" : "Create Account"}</button>}
-          {isLoading && <p>Submitting...</p>}
+          {!isLoading && <button>{isLogin ? 'Login' : 'Create Account'}</button>}
+          {isLoading && <p>Sending Request...</p>}
           <button
-            type="button"
+            type='button'
             className={classes.toggle}
             onClick={switchAuthModeHandler}
           >
-            {isLogin ? "Create new account" : "Login with existing account"}
+            {isLogin ? 'Create new account' : 'Login with existing account'}
           </button>
         </div>
       </form>
